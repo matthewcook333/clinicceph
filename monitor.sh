196b42034c69e9f86a750113d20042fd66d5ac60
Philip Davis <pdavis@hmc.edu>
2014-12-07 14:29:31 -0800

    fixes

--- monitor.sh
+++ monitor.sh
@@ -4,11 +4,12 @@ FILE_PATH=$1
 SERVERS_FILE=$2
 OUT_FOLDER=$3
 KEY_FILE=$4
+USER=$5
 
 if [ ! -d "$OUT_FOLDER" ]; then
     mkdir "$OUT_FOLDER"
 fi
 
 while read line; do
-    scp -i "$KEY_FILE $line:$FILE_PATH $OUT_FOLDER"
+    scp -i "$KEY_FILE" "$5@$line:$FILE_PATH" "$OUT_FOLDER"
 done < $SERVERS_FILE
