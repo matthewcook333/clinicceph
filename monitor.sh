50d5f7c45719c1478782f4f982f987ae50df2e04
Philip Davis <pdavis@hmc.edu>
2015-01-22 12:20:15 -0800

    ∆script_purpose

--- monitor.sh
+++ monitor.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 FILE_PATH=$1
-SERVERS_FILE=$2
+SERVER_NAME=$2
 OUT_FOLDER=$3
-KEY_FILE=$4
-USER=$5
 
 if [ ! -d "$OUT_FOLDER" ]; then
     mkdir "$OUT_FOLDER"
 fi
 
-while read line; do
-    rsync -i "$KEY_FILE" -a "$5@$line:$FILE_PATH" "$OUT_FOLDER"
-done < $SERVERS_FILE
+# Exits with status of this rsync, so missing file errors
+rsync -a "$SERVER_NAME:$FILE_PATH" "$OUT_FOLDER"
